a27cde28be865a83ef86ab79ad04904c3a7eaa47
QRImpresa <andrea.mero@hotmail.it>
2025-12-04 11:34:36 +0100

    Update logger.py

--- bot/utili/logger.py
+++ bot/utili/logger.py
@@ -1,1 +1,8 @@
+import logging
 
+def setup_logger(name=__name__):
+    logging.basicConfig(
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        level=logging.INFO,
+    )
+    return logging.getLogger(name)
